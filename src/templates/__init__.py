7c692b93464558e2d20daade774da2626ce7bb07
abhivir-42 <abhivir42@gmail.com>
2025-05-09 12:23:38 +0100

    Update templates module initialization

--- src/templates/__init__.py
+++ src/templates/__init__.py
@@ -2,7 +2,7 @@
 Template modules for agent workflows in AI Data Science.
 """
 
-from ai_data_science.templates.agent_templates import (
+from src.templates.agent_templates import (
     node_func_execute_agent_code_on_data, 
     node_func_human_review,
     node_func_fix_agent_code, 
